9ba3c3478bf2db27b9d19794506e67473649b35e
Samprit Datta <samscript106@gmail.com>
2026-08-12 06:37:05 +0530

    Client Socket created

--- server.cpp
+++ server.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main() {
-
+    //1. Server Socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0); //Requests OS to give an IPv4 TCP communication endpoint 
 
     if (serverSocket == -1) {
         cout << "Socket creation failed\n";
         return 1;
     }
-
+    //2. Bind to port 8080
     sockaddr_in serverAddress; //A box containing IP Address, Port & Protocol information
     serverAddress.sin_family = AF_INET; //this address uses IPV4
     serverAddress.sin_addr.s_addr = INADDR_ANY; //Accept connections coming to any of this computer network's interfaces 
@@ -23,14 +23,24 @@ int main() {
         return 1;
     } //binds the socket to a specific IP no. & port
 
-    listen(serverSocket, 5); //puts the server into a state where its ready to receive connection requests
-    //5 is the backlog (How many incoming connection requests can wait in the queue while our server is busy handling another connection)
-
+    
+    //3. Listen() - Wait for connection requests
     if (listen(serverSocket, 5) == -1){
         cout<<"Listen failed\n";
         return 1;
     }
+    //puts the server into a state where its ready to receive connection requests
+    //5 is the backlog (How many incoming connection requests can wait in the queue while our server is busy handling another connection)
 
-    
+    //4. Accept() - accept a client connection
+    //5. Client Socket 
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+    if(clientSocket == -1){
+        cout<<"Accept failed\n";
+        return 1;
+    }
+
+    cout<<"Client connected\n";
     return 0;
 }
