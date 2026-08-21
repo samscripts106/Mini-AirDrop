8358adaac5986d93bc7d58cef058cbf6b47da622
Samprit Datta <samscript106@gmail.com>
2026-08-22 04:30:06 +0530

    Added sendAll & recvAll function for reliable data transfer

--- client.cpp
+++ client.cpp
@@ -2,13 +2,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <fstream>
 using namespace std;
 
-int main(){
-    int clinetSocket = socket(AF_INET, SOCK_STREAM, 0);
+bool recvAll(int socket, char* data, long long totalBytes){
+    long long totalReceived = 0;
+    while(totalReceived<totalBytes){
+        int bytesRecieved = recv(socket, data+totalReceived, totalBytes-totalReceived, 0);
+        if(bytesRecieved<=0) return false;
+        totalReceived += bytesRecieved;
+    }
+    return true; //totalReceived==totalBytes
+}
 
-    if(clinetSocket == -1){
+int main(){
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(clientSocket == -1){
         cout<<"Socket creation failed\n";
         return 1;
     }
@@ -20,10 +30,46 @@ int main(){
     inet_pton(AF_INET,"127.0.0.1",&serverAddress.sin_addr);
 
     //3. Connect to server
-    if (connect(clinetSocket,(struct sockaddr*)&serverAddress, sizeof(serverAddress))==-1){
+    if (connect(clientSocket,(struct sockaddr*)&serverAddress, sizeof(serverAddress))==-1){
         cout<<"Connection failed\n";
         return 1;
     }
     cout<<"Connected to server!\n";
+
+    //4. Receive file size
+    long long fileSize;
+    // recv(clientSocket, &fileSize, sizeof(fileSize), 0); improvisation:
+    if(!recvAll(clientSocket, reinterpret_cast<char*>(&fileSize), sizeof(fileSize))){
+        cout<<"Failed to receive file size\n";
+        return 1;
+    }
+
+    cout<<"File size: "<<fileSize<<" bytes\n";
+
+    //5. Create destination file
+    ofstream file ("received.txt", ios::binary);
+
+    if(!file){
+        cout<<"Could not create file\n";
+        return 1;
+    }
+
+    //6. Receive file 
+    char buffer[4096]; //Buffer where incoming network data temporarly goes
+    long long totalReceived = 0; //How many bytes we received so far?
+
+    //Keep receiving until we reach the end of the file
+    while (totalReceived<fileSize){
+        int bytesReceived  = recv(clientSocket,buffer,sizeof(buffer),0);
+        if(bytesReceived <= 0) break; //In case something went wrong or connection closed
+
+        file.write(buffer,bytesReceived); //Write the received bytes into our new file
+        totalReceived += bytesReceived; //Keep track of how much file we received
+
+        cout<< " \rReceived: "<< totalReceived <<" / "<<fileSize<<" bytes";
+    }
+    cout<<"\n File received!\n";
+
+    file.close();
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <fstream>
 using namespace std;
+
+bool sendAll(int socket, const char* data, long long totalBytes){
+    long long totalSent = 0;
+    while(totalSent<totalBytes){
+        int bytesSent = send(socket, data+totalSent, totalBytes-totalSent, 0); 
+        if(bytesSent<=0) return false; 
+        totalSent += bytesSent;
+    }
+    return true; //totalSent==totalBytes
+}
 
 int main() {
     //1. Server Socket
@@ -30,19 +40,58 @@ int main() {
         return 1;
     }
     cout<<"Server is listening on port 8080...\n";
-    
+
     //puts the server into a state where its ready to receive connection requests
     //5 is the backlog (How many incoming connection requests can wait in the queue while our server is busy handling another connection)
 
     //4. Accept() - accept a client connection
-    //5. Client Socket 
     int clientSocket = accept(serverSocket, nullptr, nullptr);
 
     if(clientSocket == -1){
         cout<<"Accept failed\n";
         return 1;
     }
-
     cout<<"Client connected\n";
+
+    //5. Open file
+    ifstream file ("test.txt", ios::binary);
+    if(!file){
+        cout<<"Could not open file\n";
+        return 1;
+    }
+
+    //6. Find file size
+    file.seekg(0, ios::end); 
+    long long fileSize = file.tellg(); 
+    file.seekg(0, ios::beg);
+
+    //7. Send the file size
+    // send(clientSocket, &fileSize, sizeof(fileSize), 0); improvisation:
+    if(!sendAll(clientSocket, reinterpret_cast<char*> (&fileSize), sizeof(fileSize))){
+        cout<<"Failed to send the file size\n";
+        return 1;
+    }
+
+    //8. Send file
+    char buffer[4096]; //Temporrary memory area (We will read a small part of file into this)
+
+    //Keep reading the file until we reach the end
+    while(file){
+        file.read(buffer,sizeof(buffer)); //Read upto 4096 bytes from the file & put it into buffer
+        streamsize bytesRead = file.gcount(); //How many bytes did we actually read (usually 4096 but the file chunk may be smaller)
+
+        //Only send if we actually read something
+        if(bytesRead>0){
+            // send(clientSocket, buffer, bytesRead, 0); improvisation:
+            if(!sendAll(clientSocket, buffer, bytesRead)){
+                cout<<"Failed to send file\n";
+                return 1;
+            }
+        }
+    }
+
+    cout<<"File sent succesfully!\n";
+
+    file.close();
     return 0;
 }
